8083c42e6176a35c580b73d1a2d1f31a33ac1002
AR <arseneyr@users.noreply.github.com>
2020-05-06 18:20:50 -0700

    Updates

---
+++ precache-manifest.1e324b550c068224f74b871617683d16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d02fa5de1e807d5dcc62970bd61a4cf7",
+    "url": "/speakerbox/af60f403372b10a63ad9.worker.js"
+  },
+  {
+    "revision": "e958ae9ae7fad6fc91ff66306df76bfd",
+    "url": "/speakerbox/index.html"
+  },
+  {
+    "revision": "a71de2c9b29819d8a6ee",
+    "url": "/speakerbox/static/js/2.4c6fd8d6.chunk.js"
+  },
+  {
+    "revision": "2d28bd2000d160d628a8e86877732adf",
+    "url": "/speakerbox/static/js/2.4c6fd8d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4676687edb1087ce5d0",
+    "url": "/speakerbox/static/js/main.7ad19154.chunk.js"
+  },
+  {
+    "revision": "4fc5985553e0c3d830e0",
+    "url": "/speakerbox/static/js/runtime-main.ac053861.js"
+  },
+  {
+    "revision": "149eb67a9020d42b8932bcdc38ab6576",
+    "url": "/speakerbox/static/media/rotateIcon.149eb67a.svg"
+  }
+]);
