6a1df2ac987f0f20fbc4d6fcc58305ccdc3d48d7
arseneyr <arseneyr@users.noreply.github.com>
2021-09-09 15:46:40 -0700

    selfhost fonts + dep bump

--- src/Layout.svelte
+++ src/Layout.svelte
@@ -3,20 +3,4 @@
   import "normalize.css";
 </script>
 
-<svelte:head>
-  <link rel="preconnect" href="https://fonts.gstatic.com" />
-  <link
-    rel="stylesheet"
-    href="https://fonts.googleapis.com/icon?family=Material+Icons"
-  />
-  <link
-    rel="stylesheet"
-    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700"
-  />
-  <link
-    href="https://fonts.googleapis.com/css2?family=Lexend:wght@300;700&display=swap"
-    rel="stylesheet"
-  />
-</svelte:head>
-
 <slot />
